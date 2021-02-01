a6bae9195cf36995d425f505102297e80488e78e
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-01 02:05:59 +0200

    rename functions

--- lesson4/fetch_hubble.py
+++ lesson4/fetch_hubble.py
@@ -1,7 +1,8 @@
 import requests
+import os
 
 
-def get_hubble_image(image_id):
+def get_hubble_image(image_id, images_folder):
     url = f'http://hubblesite.org/api/v3/image/{image_id}'
 
     response = requests.get(url)
@@ -10,26 +11,27 @@ def get_hubble_image(image_id):
 
     response = requests.get(file_url, verify=False)
     response.raise_for_status()
-    with open('images/{}.{}'.format(image_id, file_url.split('.')[-1]), 'wb') as file:
+    with open('{}/{}{}'.format(images_folder, image_id, os.path.splitext(file_url)[-1]), 'wb') as file:
         file.write(response.content)
 
 
-def get_image_from_collection(collection_name):
+def get_images_from_collection(collection_name, images_folder):
     collection_url = f'http://hubblesite.org/api/v3/images/{collection_name}'
 
     response = requests.get(collection_url)
     response.raise_for_status()
-    id_list = [file_id['id'] for file_id in response.json()]
-    print(id_list)
-    for file_id in id_list:
+    images_id = [file_id['id'] for file_id in response.json()]
+    print(images_id)
+    for file_id in images_id:
         response = requests.get(f'http://hubblesite.org/api/v3/image/{file_id}')
         response.raise_for_status()
         file_url = '{}{}'.format('https:',
                                  [image_files['file_url'] for image_files in response.json()['image_files']][-1])
         response = requests.get(file_url, verify=False)
         response.raise_for_status()
-        with open('images/{}{}.{}'.format(collection_name,
-                                          file_id,
-                                          file_url.split('.')[-1]
-                                          ), 'wb') as file:
+        with open('{}/{}{}{}'.format(images_folder,
+                                     collection_name,
+                                     file_id,
+                                     os.path.splitext(file_url)[-1]
+                                     ), 'wb') as file:
             file.write(response.content)
