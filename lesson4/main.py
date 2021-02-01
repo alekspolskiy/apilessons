ff23c1a06edf90c31f8a0d49fb63a4bcf6d995fe
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-01 02:06:54 +0200

    modified code

--- lesson4/main.py
+++ lesson4/main.py
@@ -1,65 +1,61 @@
 import os
 from PIL import Image
 import argparse
+import shutil
 import sys
 from dotenv import load_dotenv
 
-sys.path.append(os.path.join(sys.path[0], "../"))
 from instabot import Bot
-from fetch_spacex import fetch_spacex_last_launch, get_links
-from fetch_hubble import get_hubble_image, get_image_from_collection
+from fetch_spacex import fetch_spacex, get_last_launch_links
+from fetch_hubble import get_hubble_image, get_images_from_collection
+
+MAX_SIZE = 1080
 
 
-def load_photo(login, password):
-    parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument("-u", type=str, help="username")
-    parser.add_argument("-p", type=str, help="password")
+def load_photos(login, password, images_folder):
+    parser = argparse.ArgumentParser()
     parser.add_argument("-proxy", type=str, help="proxy")
     args = parser.parse_args()
     bot = Bot()
     bot.login(username=login, password=password,
               proxy=args.proxy)
 
-    img_list = os.listdir('images')
-    for img in img_list:
-        bot.upload_photo(f"images/{img}")
+    images = os.listdir(f'{images_folder}')
+    for img in images:
+        bot.upload_photo(f"{images_folder}/{img}")
 
 
-def resize_images():
-    img_list = os.listdir('images')
-    for img in img_list:
-        image = Image.open(f'images/{img}')
-        if image.width >= image.height:
-            if image.height > 1080:
-                image.thumbnail((1080, 1080))
-            else:
-                image.thumbnail((1080, image.height))
-        if image.width < image.height:
-            if image.width > 1080:
-                image.thumbnail((1080, 1080))
-            else:
-                image.thumbnail((image.width, 1080))
+def resize_images(images_folder):
+    images = os.listdir(images_folder)
+    for img in images:
+        image = Image.open(f'{images_folder}/{img}')
+        if image.width or image.height > MAX_SIZE:
+            image.thumbnail((MAX_SIZE, MAX_SIZE))
         if image.mode == 'RGBA':
             image = image.convert('RGB')
-        image.save("images/{}.jpg".format(img.split('.')[0]), format="JPEG")
-
-    for i in img_list:
-        image = Image.open(f'images/{i}')
-        if image.width > 1080 or image.height > 1080:
-            os.remove(f"images/{i}")
+        image.save("{}/{}.jpg".format(images_folder, os.path.splitext(img)[0]), format="JPEG")
+        print(img)
+    for i in images:
+        image = Image.open(f'{images_folder}/{i}')
+        if image.width > MAX_SIZE or image.height > MAX_SIZE:
+            os.remove(f"{images_folder}/{i}")
 
 
 def main():
+    images_folder = 'images'
     load_dotenv('.env')
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-    fetch_spacex_last_launch(get_links())
-    get_hubble_image(2)
-    get_image_from_collection('spacecraft')
-    resize_images()
-    load_photo(login, password)
-    os.remove('config')
+    login = os.getenv('INSTA_LOGIN')
+    password = os.getenv('INSTA_PASSWORD')
+    fetch_spacex(get_last_launch_links(), images_folder)
+    get_hubble_image(2, images_folder)
+    get_images_from_collection('spacecraft', images_folder)
+    resize_images(images_folder)
+    load_photos(login, password, images_folder)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        shutil.rmtree('config')
+        sys.exit()
