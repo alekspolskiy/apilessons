e60e05613fbc85f667c53fd84843e4e125301e51
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-03 23:51:55 +0200

    add predict_rub_salary function

--- lesson5/superjob.py
+++ lesson5/superjob.py
@@ -1,4 +1,5 @@
 import requests
+from hh import predict_salary
 
 
 def get_vacancies(secret_key):
@@ -13,5 +14,24 @@ def get_vacancies(secret_key):
 
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
+    vacancies_ids = []
     for vacancy in response.json()['objects']:
-        print(vacancy['profession'])
+        vacancies_ids.append(vacancy['id'])
+
+    salary_info = []
+    for id in vacancies_ids:
+        salary_info.append(predict_rub_salary_sj(id))
+    for i in salary_info:
+        print(i)
+
+
+def predict_rub_salary_sj(vacancy_id):
+    url = f'https://api.superjob.ru/2.0/vacancies/{vacancy_id}'
+    response = requests.get(url)
+    response.raise_for_status()
+    salary_info = response.json()
+    return {
+        'salary_currency': salary_info['currency'],
+        'salary_from': salary_info['payment_from'],
+        'salary_to': salary_info['payment_to']
+    }
