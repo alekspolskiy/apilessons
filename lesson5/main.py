573052f517135b043fd1b0921033ecdf3f1b8327
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-03 14:07:52 +0200

    add predict_rub_salary()

--- lesson5/main.py
+++ lesson5/main.py
@@ -18,6 +18,19 @@ def get_vacancies(url):
             }
         )
 
+
+def get_python_vacancies(url):
+    params = {
+        'text': 'программист Python',
+        'area': '1',
+        'only_with_salary': 'true',
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    for item in response.json()['items']:
+        print(predict_rub_salary(item['id'], url))
+
+
 def get_python_salaries(url):
     params = {
         'text': 'программист Python',
@@ -30,10 +43,25 @@ def get_python_salaries(url):
         print(item['salary'])
 
 
+def predict_rub_salary(vacancy_id, url):
+    url += vacancy_id
+    response = requests.get(url)
+    response.raise_for_status()
+    salary = response.json()['salary']
+    if salary['currency'] != 'RUR':
+        return None
+    if salary['from'] is None:
+        return salary['to'] * 0.8
+    if salary['to'] is None:
+        return salary['from'] * 1.2
+    return (salary['from']+salary['to'])/2
+
+
 def main():
     url = 'https://api.hh.ru/vacancies/'
     # get_vacancies(url)
     get_python_salaries(url)
+    get_python_vacancies(url)
 
 
 if __name__ == '__main__':
