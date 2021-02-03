01cdd90afb302d9b1302fb17009f14af9d2fc98b
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-03 18:24:15 +0200

    add dotenv

--- lesson5/main.py
+++ lesson5/main.py
@@ -1,10 +1,15 @@
+import os
 from hh import get_average_languages_salary
+from superjob import get_api
+from dotenv import load_dotenv
 
 
 def main():
     languages = ['C#', 'CSS', 'C++', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
-
-    get_average_languages_salary(languages)
+    load_dotenv('.env')
+    secret_key = os.getenv('SUPERJOB_SECRET_KEY')
+    # get_average_languages_salary(languages)
+    get_api(secret_key)
 
 
 if __name__ == '__main__':
