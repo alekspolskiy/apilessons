ffafd872bb3a938f20e6dfc14e419ed34d7edcc6
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-03 15:59:00 +0200

    add get_average_languages_salary

--- lesson5/main.py
+++ lesson5/main.py
@@ -1,34 +1,32 @@
 import requests
+import statistics
 
 
-def get_vacancies(url):
-    languages = ['C#', 'CSS', 'C++', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
-    for language in languages:
-        params = {
-            'text': f'программист {language}',
-            'area': '1',
-            'only_with_salary': 'true',
-        }
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        result_found = response.json()['found']
-        print(
-            {
-                language: result_found
-            }
-        )
-
-
-def get_python_vacancies(url):
+def get_vacancies(language):
+    url = 'https://api.hh.ru/vacancies/'
     params = {
-        'text': 'программист Python',
+        'text': f'программист {language}',
+        'area': '1',
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    result_found = response.json()['found']
+
+    return {language: result_found}
+
+
+def get_vacancies_salaries(language):
+    url = 'https://api.hh.ru/vacancies/'
+    params = {
+        'text': f'программист {language}',
         'area': '1',
         'only_with_salary': 'true',
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
-    for item in response.json()['items']:
-        print(predict_rub_salary(item['id'], url))
+
+    return [predict_rub_salary(item['id'], url) for item in response.json()['items'] if
+            predict_rub_salary(item['id'], url) is not None]
 
 
 def get_python_salaries(url):
@@ -54,14 +52,27 @@ def predict_rub_salary(vacancy_id, url):
         return salary['to'] * 0.8
     if salary['to'] is None:
         return salary['from'] * 1.2
-    return (salary['from']+salary['to'])/2
+    return (salary['from'] + salary['to']) / 2
+
+
+def get_average_languages_salary(languages):
+    for language in languages:
+        print({
+            language: {
+                'vacancies_found': get_vacancies(language)[f'{language}'],
+                'vacancies_processed': len(get_vacancies_salaries(language)),
+                'average_salary': int(statistics.mean(get_vacancies_salaries(language)))
+            }
+        })
 
 
 def main():
+    languages = ['C#', 'CSS', 'C++', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
     url = 'https://api.hh.ru/vacancies/'
-    # get_vacancies(url)
-    get_python_salaries(url)
-    get_python_vacancies(url)
+    # get_vacancies(url, languages)
+    # get_python_salaries(url)
+    # print(get_vacancies_salaries(languages[0]))
+    get_average_languages_salary(languages)
 
 
 if __name__ == '__main__':
