81ac4c2fe1ffbfc9dead99664f470e9db2f7d89b
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-03 23:50:58 +0200

    change logic

--- lesson5/hh.py
+++ lesson5/hh.py
@@ -21,6 +21,7 @@ def get_vacancies_salaries(language):
     pages_number = 1
 
     vacancies = []
+    vacancies_ids = []
     while page < pages_number:
         params = {
             'text': f'программист {language}',
@@ -32,35 +33,57 @@ def get_vacancies_salaries(language):
         page_response.raise_for_status()
         pages_number = page_response.json()['pages']
         page += 1
+        print(page)
         [vacancies.append(item) for item in page_response.json()['items']]
+    [vacancies_ids.append(item['id']) for item in vacancies]
+    language_salaries = []
+    for vacancy_id in vacancies_ids:
+        salary_info = predict_rub_salary_hh(vacancy_id, url)
+        salary = predict_salary(salary_info['salary_currency'],
+                                salary_info['salary_from'],
+                                salary_info['salary_to']
+                                )
+        if salary is not None:
+            language_salaries.append(salary)
 
-    return [predict_rub_salary(item['id'], url) for item in vacancies if
-            predict_rub_salary(item['id'], url) is not None]
+    return {
+        'vacancies_processed': len(language_salaries),
+        'average_salary': int(statistics.mean(language_salaries))
+    }
 
 
-def predict_rub_salary(vacancy_id, url):
+def predict_salary(salary_currency, salary_from, salary_to):
+    if salary_currency != 'RUR' and salary_currency != 'rub':
+        return None
+    if salary_from is None or salary_from == 0:
+        return salary_to * 0.8
+    if salary_to is None or salary_to == 0:
+        return salary_from * 1.2
+    return (salary_from + salary_to) / 2
+
+
+def predict_rub_salary_hh(vacancy_id, url):
     url += vacancy_id
     response = requests.get(url)
     response.raise_for_status()
     salary = response.json()['salary']
-    if salary['currency'] != 'RUR':
-        return None
-    if salary['from'] is None:
-        return salary['to'] * 0.8
-    if salary['to'] is None:
-        return salary['from'] * 1.2
-    return (salary['from'] + salary['to']) / 2
+    return {
+        'salary_currency': salary['currency'],
+        'salary_from': salary['from'],
+        'salary_to': salary['to']
+    }
 
 
 def get_average_languages_salary(languages):
     for language in languages:
-        print({
+        vacansy_info = get_vacancies_salaries(language)
+        return {
             language: {
                 'vacancies_found': get_vacancies(language)[f'{language}'],
-                'vacancies_processed': len(get_vacancies_salaries(language)),
-                'average_salary': int(statistics.mean(get_vacancies_salaries(language)))
+                'vacancies_processed': vacansy_info['vacancies_processed'],
+                'average_salary': vacansy_info['average_salary']
             }
-        })
+        }
 
 
 def get_python_salaries(url):
