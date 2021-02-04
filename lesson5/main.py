232e00235c776e1437094a098a595eb51d24943d
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-04 23:50:41 +0200

    edit print_table functions

--- lesson5/main.py
+++ lesson5/main.py
@@ -7,56 +7,45 @@ from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
 
-def print_table_sj(secret_key, languages):
+def print_tables(secret_key, languages):
     data_sj = get_average_language_salary_sj(secret_key, languages)
-    TABLE_DATA = [
+    data_hh = get_average_language_salary(languages)
+    table_data_sj = [
         ('Prog Language', 'Vacancies found', 'Vacancies processed', 'Average salary'),
     ]
-    for language in languages:
-        TABLE_DATA.append([language,
-                           data_sj[language]['vacancies_found'],
-                           data_sj[language]['vacancies_processed'],
-                           data_sj[language]['average_salary']]
-                          )
-
-    """Main function."""
-    title = 'SuperJob Moscow'
-
-    # AsciiTable.
-    table_instance = AsciiTable(TABLE_DATA, title)
-    table_instance.justify_columns[2] = 'right'
-    print(table_instance.table)
-    print()
-
-
-def print_table_hh(languages):
-    data_sj = get_average_language_salary(languages)
-    TABLE_DATA = [
+    table_data_hh = [
         ('Prog Language', 'Vacancies found', 'Vacancies processed', 'Average salary'),
     ]
-    for language in languages:
-        TABLE_DATA.append([language,
-                           data_sj[language]['vacancies_found'],
-                           data_sj[language]['vacancies_processed'],
-                           data_sj[language]['average_salary']]
-                          )
 
-    """Main function."""
+    for language in languages:
+        table_data_sj.append([language,
+                              data_sj[language]['vacancies_found'],
+                              data_sj[language]['vacancies_processed'],
+                              data_sj[language]['average_salary']]
+                             )
+        table_data_hh.append([language,
+                              data_hh[language]['vacancies_found'],
+                              data_hh[language]['vacancies_processed'],
+                              data_hh[language]['average_salary']]
+                             )
+
     title = 'SuperJob Moscow'
 
-    # AsciiTable.
-    table_instance = AsciiTable(TABLE_DATA, title)
-    table_instance.justify_columns[2] = 'right'
-    print(table_instance.table)
-    print()
+    table_instance_sj = AsciiTable(table_data_sj, title)
+    table_instance_sj.justify_columns[2] = 'right'
+    table_instance_hh = AsciiTable(table_data_hh, title)
+    table_instance_hh.justify_columns[2] = 'right'
+
+    return [table_instance_hh.table, table_instance_sj.table]
 
 
 def main():
     languages = ['C#', 'CSS', 'C++', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
     load_dotenv('.env')
     secret_key = os.getenv('SUPERJOB_SECRET_KEY')
-    print_table_sj(secret_key, languages)
-    print_table_hh(languages)
+    tables = print_tables(secret_key, ['C++'])
+    # for table in tables:
+    print(tables[1])
 
 
 if __name__ == '__main__':
