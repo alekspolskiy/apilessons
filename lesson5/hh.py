b70f327951178864b29fe60870506b2a2b584e30
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-05 02:58:53 +0200

    change logic

--- lesson5/hh.py
+++ lesson5/hh.py
@@ -3,39 +3,45 @@ import statistics
 from utils import predict_salary
 
 
-def get_vacancies_hh(language):
+def get_vacancies_hh(language, vacancies_ids_check: bool):
     url = 'https://api.hh.ru/vacancies/'
     page = 0
     pages_number = 1
 
     vacancies = []
     vacancies_ids = []
+    params = {
+        'text': f'программист {language}',
+        'area': '1',
+        'only_with_salary': 'true',
+    }
     while page < pages_number:
-        params = {
-            'text': f'программист {language}',
-            'area': '1',
-            'only_with_salary': 'true',
-            'page': page
-        }
+        params['page'] = page
         page_response = requests.get(url, params=params)
         page_response.raise_for_status()
         pages_number = page_response.json()['pages']
         page += 1
-        [vacancies.append(item) for item in page_response.json()['items']]
-    [vacancies_ids.append(item['id']) for item in vacancies]
-    return vacancies_ids
+        if not vacancies_ids_check:
+            [vacancies.append(item) for item in page_response.json()['items']]
+        else:
+            [vacancies_ids.append(item['id']) for item in page_response.json()['items']]
+
+    if vacancies_ids_check:
+        return vacancies_ids
+    return vacancies
 
 
 def get_vacancies_salaries_hh(language):
-    vacancies_ids = get_vacancies_hh(language)
+    vacancies_ids = get_vacancies_hh(language, vacancies_ids_check=True)
+    print(vacancies_ids)
     language_salaries = []
     for vacancy_id in vacancies_ids:
-        salary_info = predict_rub_salary_hh(vacancy_id)
-        salary = predict_salary(salary_info['salary_currency'],
-                                salary_info['salary_from'],
-                                salary_info['salary_to']
-                                )
-        if salary is not None:
+        salary_info = predict_rub_salary_hh(language, vacancy_id)
+        if salary_info['salary_currency'] == 'RUR':
+            salary = predict_salary(
+                                    salary_info['salary_from'],
+                                    salary_info['salary_to']
+                                    )
             language_salaries.append(salary)
 
     return {
@@ -45,16 +51,15 @@ def get_vacancies_salaries_hh(language):
     }
 
 
-def predict_rub_salary_hh(vacancy_id):
-    url = f'https://api.hh.ru/vacancies/{vacancy_id}'
-    response = requests.get(url)
-    response.raise_for_status()
-    salary = response.json()['salary']
-    return {
-        'salary_currency': salary['currency'],
-        'salary_from': salary['from'],
-        'salary_to': salary['to']
-    }
+def predict_rub_salary_hh(language, vacancy_id):
+    vacancies = get_vacancies_hh(language, vacancies_ids_check=False)
+    for vacancy in vacancies:
+        if vacancy['id'] == vacancy_id:
+            return {
+                'salary_currency': vacancy['salary']['currency'],
+                'salary_from': vacancy['salary']['from'],
+                'salary_to': vacancy['salary']['to']
+            }
 
 
 def get_average_language_salary(languages):
