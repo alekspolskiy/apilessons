48cd7c1de1b4f93460589713c6ad9ba096236501
Alex Polskyi <alekspolskyi@gmail.com>
2021-02-06 02:16:46 +0200

    corrected if

--- lesson5/utils.py
+++ lesson5/utils.py
@@ -1,7 +1,7 @@
 
 def predict_salary(salary_from, salary_to):
-    if salary_from is None or salary_from == 0:
+    if not salary_from:
         return salary_to * 0.8
-    if salary_to is None or salary_to == 0:
+    if not salary_to:
         return salary_from * 1.2
     return (salary_from + salary_to) / 2
